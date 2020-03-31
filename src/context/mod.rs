4e1e79f25aef85717c8771b3fbdd3fd39c348ea3
nnao45 <n4sekai5y@gmail.com>
2020-03-31 23:28:54 +0900

    fix

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -1,49 +1,38 @@
+use std::{convert, fmt, ops};
+
 struct MD5Context {
     state: [u32; 4],
     count: [u32; 2],
-    buffer: [char; 64]
+    buffer: [u32; 64]
 }
+
+const PADDING: [u8; 64] = [
+    0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+];
+
+/// A digest.
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub struct Digest(pub [u8; 16]);
+
 
 impl MD5Context {
     fn init() -> Self {
         MD5Context {
             state: [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476],
             count: [0, 0],
-            buffer: ['a' ;64],
+            buffer: [0 ;64],
         }
     }
+}
 
-    fn decode(&mut self, input: &[char], input_len: u32) -> Vec<u32> {
-        let mut i = 0;
-        let mut j = 0;
-        let mut result = vec![];
-        while j < input_len {
-            result[i] = input[j] | (input[j + 1] << 8) | (input[j + 2] << 16) | (input[j + 3] << 24);
-            i += 1;
-            j += 4;
-        }
-        result
-    }
-
-    fn transform(&self) {
-        let a = self.state[0];
-        let b = self.state[1];
-        let c = self.state[2];
-        let d = self.state[3];
-        let x : [u32; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    }
-
-    fn update(&mut self, input: &char, input_len: u32) {
-        let index = (self.count[0] >> 3) & 0x3F;
-        if (self.count[0] + input_len << 3) < (input_len << 3) {
-            self.count[1] + 1;
-            self.count[1] + (input_len >> 29);
-        };
-
-        let part_len = 64 - index;
-
-        if (input_len >= part_len) {
-            MD5ContextOutput::init(self.buffer, part_len)
-        }
-    }
+#[test]
+fn test_cosume() {
+    let mut context = MD5Context::init();
+    let data = "ほにゃらら";
+    context.consume(data);
+    let digest = context.compute();
+    println!("{:x}", digest)
 }
